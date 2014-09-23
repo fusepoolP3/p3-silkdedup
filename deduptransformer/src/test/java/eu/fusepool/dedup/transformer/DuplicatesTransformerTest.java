4c6f95b3b4d2755179edaa4df4d6f73c2e9ff552
Luigi Selmi <selmi_luigi@hotmail.com>
2014-09-23 17:55:51 +0200

    updated test for the transformer

--- deduptransformer/src/test/java/eu/fusepool/dedup/transformer/DuplicatesTransformerTest.java
+++ deduptransformer/src/test/java/eu/fusepool/dedup/transformer/DuplicatesTransformerTest.java
@@ -4,47 +4,69 @@ package eu.fusepool.dedup.transformer;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
 import java.util.Iterator;
 
 import org.apache.clerezza.rdf.core.Triple;
 import org.apache.clerezza.rdf.core.TripleCollection;
 import org.apache.clerezza.rdf.core.UriRef;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpStatus;
+import org.hamcrest.core.StringContains;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+
 import eu.fusepool.dedup.transformer.DuplicatesTransformer;
+import eu.fusepool.p3.transformer.server.TransformerServer;
 
 public class DuplicatesTransformerTest {
-
-	@Test
-	public void testGetSubjectObject() {
-		String testString = "<http://example.org/s><http://www.w3.org/2002/07/owl#sameAs><http://example.org/o>";
-		DuplicatesTransformer transformer = new DuplicatesTransformer(); 
-		String subject = transformer.getSubject(testString).getUnicodeString();
-		String object = transformer.getObject(testString).getUnicodeString();
-		assertEquals("http://example.org/s",subject);
-		assertEquals("http://example.org/o",object);
+	
+	final String SILK_CONFIG_FILE = "src/test/resources/silk-config-file.xml";
+	final String SOURCE_RDF_FILE = "src/main/resources/inputdata.ttl";
+	final String TARGET_RDF_FILE = "src/main/resources/inputdata.ttl";
+	private String baseUri;
+	private String rdfData;
+	
+	@Before
+    public void setUp() throws Exception {
 		
-	}
+		File rdfFile = new File(SOURCE_RDF_FILE);
+		InputStream in = new FileInputStream(rdfFile);
+        rdfData = IOUtils.toString(in);
+        in.close();
+        
+        final int port = findFreePort();
+        baseUri = "http://localhost:"+port+"/";
+        TransformerServer server = new TransformerServer(port);
+        server.start(new DuplicatesTransformer(SILK_CONFIG_FILE));
+    }
+	
 	
 	@Test
-	public void testParseResult() throws IOException {
-		DuplicatesTransformer transformer = new DuplicatesTransformer(); 
-		TripleCollection links = transformer.parseResult("src/test/resources/testlinks.nt");
-		Iterator<Triple> ilinks = links.iterator();
-		while(ilinks.hasNext()){
-			Triple triple = ilinks.next();
-			String subject = ((UriRef) triple.getSubject()).getUnicodeString();
-			String object = ((UriRef) triple.getObject()).getUnicodeString();
-			assertEquals("http://example.org/s",subject);
-			assertEquals("http://example.org/o",object);
-		}
+	public void testSilk() throws IOException {
 		
+		Response response = RestAssured.given().header("Accept", "text/turtle")
+                .contentType("text/turtle;charset=UTF-8")
+                .content(rdfData)
+                .expect().statusCode(HttpStatus.SC_OK).content(new StringContains("hello")).header("Content-Type", "text/turtle").when()
+                .post(baseUri);
 	}
 	
-	@Test
-	public void testFindDuplicates(){
-		
-	}
+	public static int findFreePort() {
+        int port = 0;
+        try (ServerSocket server = new ServerSocket(0);) {
+            port = server.getLocalPort();
+        } catch (Exception e) {
+            throw new RuntimeException("unable to find a free port");
+        }
+        return port;
+    }
 
 }
