e8213eacbd78a603ff3ad13e96959af9b22b1346
Luigi Selmi <selmi_luigi@hotmail.com>
2015-06-24 11:46:41 +0200

    test async transformer

--- src/main/java/eu/fusepool/dedup/transformer/Main.java
+++ src/main/java/eu/fusepool/dedup/transformer/Main.java
@@ -35,7 +35,7 @@ public class Main {
         TransformerServer server = new TransformerServer(arguments.getPort());
         //InputStream in = Main.class.getResourceAsStream("bla.txt");
         
-        server.start(new DuplicatesTransformerFactory());
+        server.start(new DuplicatesTransformer(true));
        
         server.join();
     }
